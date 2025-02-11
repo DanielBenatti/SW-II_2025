5d408a35c30aa2273472f82612ee793ca3d5d5f9
Daniel Benatti <124791723+DanielBenatti@users.noreply.github.com>
2025-02-11 12:47:10 -0300

    Add files via upload

---
+++ aula_1/exercicio_2/calculamedia.php
@@ -0,0 +1,15 @@
+<?php
+    $nota1 =8;
+    $nota1 =7;
+    $nota1 =9;
+
+    echo "nota 1:".$nota1."<br>";
+    echo "nota 2:".$nota2."<br>";
+    echo "nota 3:".$nota3."<br>";
+    echo "<br>";
+    if (($nota1+$nota2+$nota3)/3  >= 5) {
+        echo "Aprovado!";
+        } else {
+        echo "Reprovado!";
+        }
+?>
---
+++ aula_1/exercicio_3/listapares.php
@@ -0,0 +1,10 @@
+<?php
+    $valo = 1;
+
+    for($valo < 20){
+        if($valo%2 = 0){
+            echo $valo;
+        }
+        $valo++;
+    }
+?>
